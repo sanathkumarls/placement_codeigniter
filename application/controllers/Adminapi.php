ca554847b35773ea8c8c411391659bb6d53204f2
Sanath L S <sanathlslokanathapura@gmail.com>
2019-08-17 23:51:30 +0530

    admin api started

---
+++ application/controllers/Adminapi.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanathls
+ * Date: 2019-08-17
+ * Time: 11:16
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Adminapi extends CI_Controller
+{
+
+
+}
